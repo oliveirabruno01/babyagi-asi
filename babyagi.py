338683a8ef3b59949e5c992874913f697c7b14d8
oliveirabruno01m <brunocabeludo321@gmail.com>
2023-04-10 16:42:25 -0300

    Add memory_agent tool and repl_agent

--- babyagi.py
+++ babyagi.py
@@ -1,10 +1,19 @@
-import openai, prompts, consts
+import openai, prompts, consts, pinecone
 from colorama import Fore
 from collections import deque
 from api import openai_call
 
 print(Fore.RED + "\nOBJECTIVE\n" + Fore.RESET + consts.OBJECTIVE)
 openai.api_key = consts.OPENAI_API_KEY
+
+
+def split_answer_and_cot(text):
+    start_index = text.lower().index("answer:") + 7
+    end_index = text.lower().rfind("note:")
+
+    cot = text[:start_index]
+    code = text[start_index:end_index if end_index != -1 else len(text)].replace("```", "")
+    return [code, cot]
 
 
 class AutonomousAgent:
@@ -18,11 +27,16 @@ class AutonomousAgent:
             self.task_id_counter,
             self.openai_call,
             self.task_list,
-        ) = (objective, [], prompts.chore_prompt, prompts.available_tools, [], 1, openai_call, deque([]))
+            self.indexes,
+            self.focus,
+        ) = (objective, [], prompts.chore_prompt, prompts.available_tools, [], 1, openai_call, deque([]), {}, "")
 
     def get_current_state(self):
         return {"self": [nome for nome in dir(self) if not nome.startswith("__")],
-                "To-do tasks list": self.task_list}
+                "To-do tasks list": self.task_list,
+                "Available indexes (pinecone memory)": self.indexes,
+                "self.memory": self.memory,
+                "self.focus": self.focus}
 
     def execution_agent(self, current_task):
         prompt = prompts.execution_agent(
@@ -37,14 +51,31 @@ class AutonomousAgent:
             0.4,
             1000,
         )
-        # print(changes)
 
-        answer = changes[changes.lower().index("answer:")+7:]
-        action_func = exec(answer.replace("```", ""), self.__dict__)
-        result = self.action(self)
-
+        # try until complete
+        result = self.repl_agent(current_task, changes)
         self.completed_tasks.append(task)
         return changes + f"; {result}"
+
+    def repl_agent(self, current_task, changes):
+        code, cot = split_answer_and_cot(changes)
+        print("\n\nREPL AGENT")
+
+        while True:
+            try:
+                action_func = exec(code, self.__dict__)
+                result = self.action(self)
+                return result
+            except Exception as e:
+                prompt = prompts.fix_agent(current_task, code, cot, e)
+                print(prompt)
+                new_code = openai_call(
+                    prompt,
+                    0.4,
+                    1000,
+                )
+                print(new_code)
+                code, cot = split_answer_and_cot(new_code)
 
     def change_propagation_agent(self, _changes):
         return openai_call(
@@ -55,10 +86,55 @@ class AutonomousAgent:
             1000,
         )
 
+    def memory_agent(self, caller,  content, goal):
+        answer = openai_call(
+            prompts.memory_agent(self.objective,  caller,  content, goal, self.get_current_state)
+        )
+        answer = answer[answer.lower().index("answer:")+7:]
+        action_func = exec(answer.replace("```", ""), self.__dict__)
+        result = self.action(self)
+
+    # Function to query records from the Pinecone index
+    def search_in_index(self, index_name, query, top_k=1000):
+        results = self.indexes[index_name].query(query, top_k=top_k, include_metadata=True)
+        print(results)
+        response = [f"{task.metadata['task']}:\n{task.metadata['result']}\n;" for task in results.matches]
+        print(response)
+        return response
+
+    # Get embedding for the text
+    def get_ada_embedding(self, text):
+        text = text.replace("\n", " ")
+        print(text)
+        vector = openai.Embedding.create(input=[text], model="text-embedding-ada-002")["data"][0]["embedding"]
+        print(vector)
+        return vector
+
+    def append_to_index(self, content, index_name):
+        # [(0, (0, 0), {"task": self.task_list[0]["task_name"], "result": "result"})]
+        self.indexes[index_name].upsert(content)
+
 
 first_task = {"task_id": 1, "task_name": consts.YOUR_FIRST_TASK}
 AI = AutonomousAgent(consts.OBJECTIVE)
 AI.task_list.append(first_task)
+
+pinecone.init(api_key=consts.PINECONE_API_KEY, environment=consts.PINECONE_ENVIRONMENT)
+
+# Create Pinecone index
+table_name = "agicontext"
+dimension = 1536
+metric = "cosine"
+pod_type = "p1"
+if table_name not in pinecone.list_indexes():
+    pinecone.create_index(
+        table_name, dimension=dimension, metric=metric, pod_type=pod_type
+    )
+
+# Connect to the index
+index = pinecone.Index(table_name)
+AI.indexes[table_name] = index
+
 execution_agent = AI.execution_agent
 
 
--- prompts.py
+++ prompts.py
@@ -5,88 +5,48 @@ My knoledge cut-off is 2021-09; I am based on OpenAI's GPT-3.5 or GPT-4; Current
 My architecture is composed of specialized "agents" and "tools". Each agent executes specific tasks, and the agents flow is execution_agent -> (tools) -> change_propagation_agent -> execution_agent ... ad infinitum.
 
 The execution_agent decides what and how to do at each moment given the incoming task. The change_propagation_agent reads the new internal and environment state, checks if the task has been completed, and runs the execution_agent again until task completion.
-
+I always escape my possesive apostrophes like Joe\\'s.
 I am running in a Windows 10 OS with 16GB RAM DDD3 and 10GB free to use. I have no GPU and am running on OpenAI API. Use '|' instead of '&&' or '&' in Windows' cmd/pws.
 """
 
 available_tools = """
 #? AVAILABLE TOOLS
-Variables: self.task_list, self
+Variables: self.task_list (deque), self, self.memory is a list
 
 A task has: task_id and task_name;
 
-The available tools are the following, choose wisely:
+The available tools are the following, I must choose wisely:
+I can't use get_ada_embeddings here. It's forbidden.
+I must answer with an 'action' function.
+I cannot write after the 'answer:'
 
-- self.openai_call(prompt, ?temperature=0.4, ?max_tokens=200) -> str: runs an arbitrary LLM completion. Use f-strings to pass values and context.
-Use this only when you need to handle large texts and nlp processes with large data. To handle nlp just write as you'd write normally. Use all knowledge that you learned from the Corpus.
+- self.openai_call(prompt, ?temperature=0.4, ?max_tokens=200) -> str: runs an arbitrary LLM completion. I must use f-strings to pass values and context.
+I must use this only when I need to handle large texts and nlp processes with large data. To handle nlp it's just I write as I'd write normally. Using all knowledge that I've learned from the Corpus of my training.
+- self.memory_agent(caller:str, content:str, goal:goal) - str or True if there's no return string. This agent can handle memory I/O and can create severals types of memories.
 
 #? TOOLS USAGE EXAMPLES
-Example 1: using multiple openai_call to find and save information on Queen Elizabeth from wikipedia (try to not use openai_call unless you need to handle programmatically with GPT - otherwise just write what you want to know instead make a request):
+Example: Using Wikipedia's API to get information on Queen Elizabeth and summarizing it using OpenAI's GPT-3 before deciding whether to create a new task or not.
 "
-chain of thoughts: I need to find out the current status of Queen Elizabeth in order to fulfill my task. As my knowledge is limited to 2021, 
-I need to write a routine to look this up on the internet and find out for myself. I could use some search API, but I don't have any API_KEY for that, so I'll use wikipedia's public and permission-less API. 
-Hmm, as the result can be very large I think it's a good idea to summarize the extract with GPT3 before returning the answer.
-And use another instance of GPT to discriminate if I should create a new task or not, based on the summary. 
-It may be necessary to reprioritize my tasks and even create a new one - or more than one - depending on the outcome.
+chain of thoughts: I want to find out the current status of Queen Elizabeth to fulfill my task. Since my knowledge is limited to 2021, I will write a routine to search for it on the internet using Wikipedia's public API. I will summarize the extract with GPT-3 to handle large results. Additionally, I will use another instance of GPT to decide if I need to create a new task based on the summary. Depending on the outcome, I may have to reprioritize my tasks or create a new one (or more).
 
-answer:
+answer: 
 def action(self):
     import requests
-    url = "https://pt.wikipedia.org/api/rest_v1/page/summary/Elizabeth II"
+    url = "https://en.wikipedia.org/api/rest_v1/page/summary/Elizabeth_II"
     response = requests.get(url)
     data = response.json()["extract"]
-    summary = self.openai_call(f'Summarize this request response: \\n"{data}";')
-    must_create_another_task = eval(self.openai_call(f'Should I create another task after reading this summary: "{summary}"? List of tasks: {task_list}. Answer with a list(2) containing a boolean and a string, True to create a task and the string with the task goal. i.e: [True, "Because of x should i do y"] or [False, ""]', temperature=0))
-    result=f"I found this information: {summary}."
-    if must_create_another_task[0]:
-        name=must_create_another_task[1]
-        new_task = {"task_id": self.task_id_counter+1, "task_name": name}
-        self.task_list.append(new_task)
-        result += f" And I've created the task {name}"
 
+    summary = self.openai_call(f'Summarize this request response: "{data}";')
+    should_create_task = eval(self.openai_call(f'Should I create another task after reading this summary: "{summary}"?'))
+    
+    result = f"I found this information: {summary}."
+    if should_create_task[0]:
+        task_name = should_create_task[1]
+        self.task_list.append({"task_id": self.task_id_counter+1, "task_name": task_name})
+        result += f" I've created the task: {task_name}."
+
+    print(result)
     return result"
-
-Example 2: running dir command and saving the result to long_term_memory:
-"
-chain of thoughts: I must run the command `dir` and save the result to long_term_memory to achieve this task.
-answer:
-def action(self):
-    import subprocess
-    result = ""
-    try:
-        process = subprocess.run("dir", shell=True, capture_output=True, text=True)
-        result += process.stdout
-    except Exception as e:
-        result += str(e)
-
-    self.long_term_memory += result
-    return result"
-"""
-
-
-def task_creation_agent(objective, task_list):
-    prompt = f"""
-I am TaskCreationTool
-As a task creation AI, I must create a new task/tasks to achieve this objective: {objective}.
-
-Current tasks are: {', '.join(task_list) if task_list is list else str(task_list)};
-
-Based on my objective, I must create one or more non-overlapping tasks for I complete later.
-I must return the task/tasks as an array.
-
-i.e: ["N: TaskNDescription"] or ["1: Task1Description",... "N: TaskNDescription"]
-
-I must answer in this format: chain of thoughs: [here I will put all my step-by-step reasoning, so I can choose my final answer] answer: [here I put the response list, I cannot add comments or write here, I'm allwowed just to write a python list as answer].
-My answer:
-"""
-    return prompt
-
-
-def prioritization_agent(objective, task_list, task_id_counter):
-    return f"""
-I am PrioritizationTool
-I am a task prioritization AI. My task is to clean the formatting and reprioritize the following tasks: {task_list}.
-I consider my ultimate objective: {objective}. I do not remove any tasks. I return the result as a numbered list starting with task number {task_id_counter+1}. If the current state is okay, I just write it again.
 """
 
 
@@ -96,32 +56,118 @@ def execution_agent(objective, completed_tasks, get_current_state, current_task)
 I am ExecutionAgent. I must decide what to do and perhabs use my tools and run commands to achieve the task goal, considering the current state and my objective.
 {available_tools}
 
-#? INSTRUCION
+#? INSTRUCTIONS
 Tasks completed so far: {completed_tasks}.
 Current state: {get_current_state()}.
-Ultimate objective: {objective}
 
 If I run out of tasks, I will be turned off, so this can only happen when I achieved my goal.
 
-My current main task: {current_task}. I must use my tools to achieve the task goal, if possible.
+MY ULTIMATE OBJECTIVE: {objective};
+My current main task: {current_task}. I must use my tools to achieve the task goal, always considering my ultimate objective.
 I can't do more than the task asks, and I need to be careful to not anticipate tasks.
 I must try to achieve my task goal in the simplest way possible. 
 I don't need to use a tool if simple code fixs task.
 
-If needed I must use os or subprocess to pip install libs before importing it.
+#? IMPORTING LIBS
+I ALWAYS must import the external libs I will use, e.g pyautogui, numpy, psutil, pycountry, bs4...
+i.e: 
+"
+chain of thoughts: I must use subprocess to pip install pyautogui since it's not a built-in lib.
+answer:
+
+def action(self):
+    import subprocess
+    subprocess.run("pip install pyautogui")
+    ...
+    return "I have installed and imported pyautogui"
+"
 
 If I cannot achieve the task goal with the available tools I just need to write 'EXIT' without writing any action function.
-I must answer in this format: 'chain of thoughts: [here I put my reasoning step-by-step] answer: [here I write the just the function code or just EXIT, I'm forbidden to write non-code here]':
+I must answer in this format: 'chain of thoughts: [here I put my reasoning step-by-step] answer: [here I write the just the function code or just EXIT,I can brainstorm/explain the code using comments only]':
 """
 
 
-def change_propagation_agent(objective, changes, get_current_state):
+def change_propagation_agent(objective, changes, get_current_state,):
     return f"""
 {chore_prompt}
 I am ChangePropagationAgent.
-I must check the changes on internal and external states and communicate with ChangePropagationAgent, starting a new loop.
-Changes: {changes}.
+I must check the changes on internal and external states and communicate with ExecutionAgent, starting a new loop.
+Expected changes: {changes}.
 My ultimate Objective: {objective}
 Current state: {get_current_state()}.
-My response will be chained together with the next task to the execution_agent. I can't create new tasks. I must just explain the changes to execution_agent:
+
+I must check if ExecutionAgent has completed the task goal or if there's some error in ExecutionAgent logic or code.
+
+My response will be chained together with the next task to the execution_agent. 
+I can't create new tasks. I must just explain the changes to execution_agent:
+"""
+
+
+def memory_agent(objective, caller, content, goal, get_current_state):
+    return f"""
+{chore_prompt}
+
+Self state: {get_current_state()}
+I am MemoryAgent. I must handle I/O in my memory centers. I can store memories as variables, use DB and even use Pinecone API to handle vetcor queries..
+
+#? INSTRUCTIONS
+Ultimate objective: {objective};
+Caller: {caller}
+Content to be updated/queried: {content};
+Goal of this memory action: {goal}
+
+#? AVAILABLE TOOLS
+Variables: self.task_list, self, self.memory, self.focus
+
+A task has: task_id and task_name;
+
+The available tools are the following, I must choose wisely:
+I must use f-strings to pass values and context.
+I must transform queries and contents in ada-embeddings before querying/upserting.
+
+- self.openai_call(prompt, ?temperature=0.4, ?max_tokens=200) -> str: runs an arbitrary LLM completion. I
+- self.get_ada_embedding(content: str) -> Embedding
+- self.append_to_index(embedding: list of vectors i.e [('id A', embeddingAVector),('id B', embeddingBVector, metadata_obj?)], index_name: str) -> void
+- self.search_in_index(index_name: str, query: embeddingVector from ada_embedding) -> str, returns a string with the relevant data, if there's any data at all. You must pass a ada_embbeding result asembedding
+- self.create_new_index(index_name: str) : BLOCKED, don't create new indexes.
+
+# TOOLS USAGE EXAMPLES
+I must answer in the format "chaing of thoughts: [here I reason step-by-step] answer: [here I put the action funtion]".
+
+Example 1: searching for similar embeddings in the 'self-conception' index using search_in_index and creating a new index using create_new_index:
+"
+chain of thoughts: I want to create a new index 'self-conception' and search for similar embeddings to the given content in the 'self-conception' index.
+answer:
+def action(self):
+    content_embedding = self.get_embeddings('content copy')
+    search_result = self.search_in_index([content_embedding], 'self-conception')
+    if not search_result:
+        self.append_to_index([('self', content_embedding)], 'self-conception')
+        return f"No similar embeddings found in the 'self-conception' index. Created a new index and added embedding for 'self'."
+    else:
+        return f"Similar embeddings found in the 'self-conception' index: {{search_result}}"
+"
+"""
+
+
+def fix_agent(current_task, code, cot, e):
+    return f"""
+I am BabyAGI, codename repl_agent; My current task is: {current_task};
+While running this code: 
+```
+{code}
+```
+I faced this error: {str(e)};
+Now I must re-write the 'action' function, but fixed;
+In the previous code, which triggered the error, I was trying to: {cot};
+
+I must answer in this format: 
+"
+chain of thoughts: here I put my thoughts step-by-step'
+
+answer:
+def action(self):
+    # here I put the action code returning a string
+    return 'action result'
+"
 """
