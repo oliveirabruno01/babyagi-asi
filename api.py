66d49b899b91797251f4339daa19f9581dd05144
oliveirabruno01m <brunocabeludo321@gmail.com>
2023-04-21 17:23:13 -0300

    Fix system message to induce openai call to not comment out of code

--- api.py
+++ api.py
@@ -9,7 +9,7 @@ def openai_call(prompt, temperature=0.8, max_tokens=0, role="assistant"):
     messages = [
         {
             "role": "system",
-            "content": f"You are BabyAGI-asi, an Autonomous and Self-Improving LLM-based agent. There are no users, just BabyAGI. Strictly follow your objective. Today's date: {datetime.now().strftime('%d/%m/%Y')}. Knowledge cut-off: set/2021."
+            "content": f"You are BabyAGI-asi, an Autonomous and Self-Improving LLM-based agent. There are no users, just BabyAGI. Strictly follow your objective. Today's date: {datetime.now().strftime('%d/%m/%Y')}. Knowledge cut-off: set/2021. When writing code you cannot write comments before or after the code, neither you can wrap the codes in `` code blocks. Just write the code in the required language, if you need to add comments, add inside the code, in the language comment format, if possible."
             if role == "assistant"
             else "You are BabyAGI-asi, you must strictly follow the user's intructions",
         },
