4ae86915ffca4a0466ad47f7eb8d32488f4aa8aa
jake354 <klostreich3@gmail.com>
2023-05-16 21:04:58 -0600

    Update common_utils.py
    Adds an implementation for Bard API to common_utils.py

--- src/common_utils.py
+++ src/common_utils.py
@@ -1,9 +1,28 @@
 import tiktoken, json, openai
 from datetime import datetime
 from src import consts
-
-
+from bardapi import Bard
+import os
+import re
 encoding = tiktoken.encoding_for_model("gpt-3.5-turbo" if not consts.USE_GPT4 else "gpt-4")
+
+
+os.environ['_BARD_API_KEY']="Your api key here, remember to include the period."
+
+def bard_api_call(prompt, max_tokens=0, role="assistant"):
+    messages = [
+        {
+            "role": "system",
+            "content": f"You are BabyAGI-asi, an Autonomous and Self-Improving LLM-based agent. There are no users, just BabyAGI. Strictly follow your objective. Today's date: {datetime.now().strftime('%d/%m/%Y')}. Knowledge cut-off: set/2021. When writing code you cannot write comments before or after the code, neither you can wrap the codes in `` code blocks. Just write the code in the required language, if you need to add comments, add inside the code, in the language comment format, if possible."
+            if role == "assistant"
+            else "You are BabyAGI-asi, you must strictly follow the user's instructions",
+        },
+        {"role": role, "content": prompt},
+    ]
+    response = Bard().get_answer(str(messages))
+    text = response['content'].strip()
+    return text
+
 
 
 def openai_call(prompt, temperature=0.8, max_tokens=0, role="assistant"):
